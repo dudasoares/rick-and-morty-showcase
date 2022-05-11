41ccbb466c4c1983cc083e0c9b568f0461c5e4a9
dudasoares <dudaserpacamboim@gmail.com>
2022-05-11 14:32:20 -0300

    Initial Commit

---
+++ rick-and-morty-showcase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rick-and-morty-showcase
+//
+//  Created by Eduarda Soares Serpa Camboim on 11/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
