a4baf7890793f900d2b68348ae9c3eaa2b3d7795
dudasoares <dudaserpacamboim@gmail.com>
2022-05-11 16:42:22 -0300

    Data connection with layout

--- rick-and-morty-showcase/View/ViewController/DetailsViewController.swift
+++ rick-and-morty-showcase/View/ViewController/DetailsViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class DetailsViewController: UIViewController {
+class DetailsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var characterDetails: Character?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        tableView.dataSource = self
+        tableView.delegate = self
+        
     }
+    
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "detailsCell", for: indexPath) as! DetailsTableViewCell
+        
+            if let character = characterDetails {
+
+            
+                let url = URL(string: character.image_path)
+
+                let task = URLSession.shared.dataTask(with: url!) {(data, response, error) in
+
+                    DispatchQueue.main.async {
+                        cell.characterImage.image = UIImage(data: data!)!
+                        cell.characterImage.layer.cornerRadius = 10
+
+                    }
+            }
+            task.resume()
+            
+                cell.characterName.text = character.name
+                cell.characterSpecie.text = " 🔹 Specie: \(character.specie)"
+                cell.characterStatus.text = " 🔹 Status: \(character.status)"
+                cell.characterGender.text = " 🔹 Gender: \(character.gender)"
+    
+        }
+        
+        
+        
+        return cell
+    }
+    
 
 }
--- rick-and-morty-showcase/View/ViewController/ViewController.swift
+++ rick-and-morty-showcase/View/ViewController/ViewController.swift
@@ -7,14 +7,86 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
     
+    var characters: [Character] = []
+    
+    let rickAndMortyAPI = CharactersAPI()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        rickAndMortyAPI.requestCharacters { (characters) in
+            self.characters = characters
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return characters.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "charactersCell", for: indexPath) as! CharactersTableViewCell
+        
+
+        let character = characters[indexPath.row]
+
+        let url = URL(string: character.image_path)
+
+        let task = URLSession.shared.dataTask(with: url!) {(data, response, error) in
+
+            DispatchQueue.main.async {
+                cell.characterImage.image = UIImage(data: data!)!
+                cell.characterImage.layer.cornerRadius = 10
+
+            }
+        }
+        task.resume()
+        
+        cell.characterName.text = character.name
+        cell.characterSpecie.text = character.specie
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        
+        // Lembrem-se de colocar um booleano pra controlar as requisições
+        // E lembrem-se de controlarem em que página estamos para pedirmos apenas a próxima
+        if (indexPath.row == characters.count - 1) {
+            rickAndMortyAPI.requestCharacters(page: 2) { (characters) in
+                self.characters.append(contentsOf: characters)
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "toDetail", sender: indexPath)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "toDetail", let indexPath = sender as? IndexPath {
+            let character = characters[indexPath.row]
+            guard let destination = segue.destination as? DetailsViewController else { return }
+            destination.characterDetails = character
+        }
+    }
+
+
 }
+    
 
